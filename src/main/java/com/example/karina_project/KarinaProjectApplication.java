964d785769498191a33227b11c878be6c28866ac
JSH-631 <newred02@handong.ac.kr>
2025-08-12 14:00:02 +0900

    project setting

---
+++ src/main/java/com/example/karina_project/KarinaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.karina_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarinaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarinaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/karina_project/KarinaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karina_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarinaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
