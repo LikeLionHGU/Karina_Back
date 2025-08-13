2054485ac07c9fa81d0ff19e4a06b8f979e75652
byoungchan-kang <kbc4249@gmail.com>
2025-08-13 16:46:23 +0900

    pakage add

--- src/main/java/com/example/karina_project/repository/UserRepository.java
+++ src/main/java/com/example/karina_project/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.karina_project.repository;
 
+import com.example.karina_project.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
