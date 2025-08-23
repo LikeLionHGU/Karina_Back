0c887925294c45822c622122ff30f095dec9b914
JSH-631 <newred02@handong.ac.kr>
2025-08-23 22:50:48 +0900

    delete unusage import

--- src/main/java/com/example/karina_project/repository/MatchingRepository.java
+++ src/main/java/com/example/karina_project/repository/MatchingRepository.java
@@ -3,13 +3,10 @@ package com.example.karina_project.repository;
 import com.example.karina_project.domain.Matching;
 import com.example.karina_project.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
