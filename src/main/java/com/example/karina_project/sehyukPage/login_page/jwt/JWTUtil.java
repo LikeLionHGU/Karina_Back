cf19da6dc704bba8c7db83e448a611ebfe866e9c
JSH-631 <newred02@handong.ac.kr>
2025-08-24 17:49:25 +0900

    edit GetFisherMyPageService & jwtToken isExpired Function

--- src/main/java/com/example/karina_project/sehyukPage/login_page/jwt/JWTUtil.java
+++ src/main/java/com/example/karina_project/sehyukPage/login_page/jwt/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.example.karina_project.sehyukPage.login_page.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -31,9 +32,19 @@ public class JWTUtil {
     }
 
     public Boolean isExpired(String token) {
-
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        try {
+            Date expiration = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload()
+                    .getExpiration();
+            return expiration.before(new Date());
+        } catch (ExpiredJwtException e) {
+            return true;
+        }
     }
+
 
     public String createJwt(String username, String role, Long userId, String realUserName, Long expiredMs) {
 
